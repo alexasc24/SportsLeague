88278c0155d4371d6f1595894352df6c301fb1de
alexasc24 <alexa.msc24@gmail.com>
2026-05-19 23:40:29 -0500

    data seeder

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -9,6 +9,7 @@ using SportsLeague.Domain.Interfaces.Repositories;
 using SportsLeague.Domain.Interfaces.Services;
 
 using SportsLeague.Domain.Services;
+using SportsLeague.DataAccess.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -76,6 +77,24 @@ builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
+// ── Data Seeder ── 
+
+using (var scope = app.Services.CreateScope())
+
+{
+
+    var context = scope.ServiceProvider
+
+        .GetRequiredService<LeagueDbContext>();
+
+
+
+    await context.Database.MigrateAsync(); // Crea la BD + aplica migraciones 
+
+    await DataSeeder.SeedAsync(context);
+
+}
 
 
 // ── Middleware Pipeline ──
