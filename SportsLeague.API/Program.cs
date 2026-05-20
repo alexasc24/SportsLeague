7f4d381b7bd8b68bdeefc796d9cb614e9586ab08
alexasc24 <alexa.msc24@gmail.com>
2026-05-19 23:02:50 -0500

    match result, goals, cards

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using SportsLeague.DataAccess.Context;
 
 using SportsLeague.DataAccess.Repositories;
-
+using SportsLeague.Domain.Helpers;
 using SportsLeague.Domain.Interfaces.Repositories;
 
 using SportsLeague.Domain.Interfaces.Services;
@@ -37,6 +37,12 @@ builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
 builder.Services.AddScoped<ITournamentTeamRepository, TournamentTeamRepository>();
 builder.Services.AddScoped<ITournamentSponsorRepository, TournamentSponsorRepository>();
 builder.Services.AddScoped<IMatchRepository, MatchRepository>();
+builder.Services.AddScoped<IMatchResultRepository, MatchResultRepository>();
+
+builder.Services.AddScoped<IGoalRepository, GoalRepository>();
+
+builder.Services.AddScoped<ICardRepository, CardRepository>();
+
 // ── Services ──
 
 builder.Services.AddScoped<ITeamService, TeamService>();
@@ -48,7 +54,9 @@ builder.Services.AddScoped<ISponsorService, SponsorService>();
 
 builder.Services.AddScoped<ITournamentService, TournamentService>();
 builder.Services.AddScoped<IMatchService, MatchService>();
+builder.Services.AddScoped<IMatchEventService, MatchEventService>();
 
+builder.Services.AddScoped<MatchValidationHelper>();
 
 // ── AutoMapper ──
 
